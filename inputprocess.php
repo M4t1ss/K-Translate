54e01a9343c27677648e075f8618ebab525deffb
Matīss Rikters <matiss_rikters@hotmail.com>
2016-01-27 20:55:12 +0200

    make it all wider...

--- inputprocess.php
+++ inputprocess.php
@@ -117,26 +117,27 @@ if(isset($parsed) && $parsed != ""){
 ?>
 	<div class="mt">
 		MT 1:<br/>
-		<textarea class="form-control" name="mt1" placeholder="Required"></textarea><br/>
+		<textarea style="width:604px;" class="form-control" name="mt1" placeholder="Required"></textarea><br/>
 	</div>
 
 	<div class="mt">
 		MT 2:<br/>
-		<textarea class="form-control" name="mt2" placeholder="Required"></textarea><br/>
+		<textarea style="width:604px;" class="form-control" name="mt2" placeholder="Required"></textarea><br/>
 	</div>
 	<br style="clear: both;"/>
 
 	<div class="mt">
 		MT 3:<br/>
-		<textarea class="form-control" name="mt3" placeholder="Optional"></textarea><br/>
+		<textarea style="width:604px;" class="form-control" name="mt3" placeholder="Optional"></textarea><br/>
 	</div>
 
 	<div class="mt">
 		MT 4:<br/>
-		<textarea class="form-control" name="mt4" placeholder="Optional"></textarea><br/>
+		<textarea style="width:604px;" class="form-control" name="mt4" placeholder="Optional"></textarea><br/>
 	</div>
 
 	<br style="clear: both;"/>
 	<input type="hidden" name="id" value="inputresult"/>
 	<input style="margin-left:5px;" type="submit" class="btn btn-sm btn-default" value="Combine!"/>
 </form>
+<br/><br/>

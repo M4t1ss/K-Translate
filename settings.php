fa6810b4960e357ede52126b66d710e2debcb31e
Matīss Rikters <matiss_rikters@hotmail.com>
2016-07-04 11:40:48 +0300

    Create a settings file for the first time

--- settings.php
+++ settings.php
@@ -3,6 +3,25 @@ class MyConfig
 {
     public static function read($filename)
     {
+		if(!is_file($filename)){
+			// Create a settings file for the first time
+			file_put_contents($filename, "<?php return array (
+  'en_gram' => '',
+  'en_lm' => '',
+  'lv_gram' => '',
+  'lv_lm' => '',
+  'de_gram' => '',
+  'de_lm' => '',
+  'fr_gram' => '',
+  'fr_lm' => '',
+  'google_key' => '',
+  'yandex_key' => '',
+  'bing_id' => '',
+  'bing_se' => '',
+  'hugo_pw' => '',
+  'hugo_em' => '',
+) ;");
+		}
         $config = include $filename;
         return $config;
     }
